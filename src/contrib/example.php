d1ca619a7bf03e9ba68cd0da4e5e93f9622bd893
Nigel McNie <nigel@catalyst.net.nz>
2005-10-18 03:21:05 +0000

    Commented example file much better, so people can use it as an example better.

--- src/contrib/example.php
+++ src/contrib/example.php
@@ -1,12 +1,17 @@
 <?php
-
 /**
  * GeSHi example script
- * --------------------
+ *
  * Just point your browser at this script (with geshi.php in the parent directory,
  * and the language files in subdirectory "../geshi/")
+ * 
+ * @author  Nigel McNie
+ * @version $Id$
  */
 
+// Rudimentary checking of where GeSHi is. In a default install it will be in ../, but
+// it could be in the current directory if the include_path is set. There's nowhere else
+// we can reasonably guess.
 if (!@include '../geshi.php') {
     if (!@include 'geshi.php') {
         die('Could not find geshi.php - make sure it is in your include path!');
@@ -26,21 +31,55 @@ if ( isset($_POST['submit']) )
 		$_POST['source'] = implode('', @file($path . 'geshi/' . $_POST['language'] . '.php'));
 		$_POST['language'] = 'php';
 	}
-	$geshi = new GeSHi($_POST['source'], $_POST['language']);
+    
+    // Here's a free demo of how GeSHi works.
+    
+    // First the initialisation: source code to highlight and the language to use. Make sure
+    // you sanitise correctly if you use $_POST of course - this very script has had a security
+    // advisory against it in the past because of this. Please try not to use this script on a
+    // live site.
+	$geshi =& new GeSHi($_POST['source'], $_POST['language']);
+    
+    // Use the PRE header. This means less output source since we don't have to output &nbsp;
+    // everywhere. Of course it also means you can't set the tab width.
 	$geshi->set_header_type(GESHI_HEADER_PRE);
+    
+    // Enable CSS classes. You can use get_stylesheet() to output a stylesheet for your code. Using
+    // CSS classes results in much less output source.
 	$geshi->enable_classes();
+    
+    // Enable line numbers. We want fancy line numbers, and we want every 5th line number to be fancy
 	$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 5);
+    
+    // Set the style for the PRE around the code. The line numbers are contained within this box (not
+    // XHTML compliant btw, but if you are liberally minded about these things then you'll appreciate
+    // the reduced source output).
 	$geshi->set_overall_style('color: #000066; border: 1px solid #d0d0d0; background-color: #f0f0f0;', true);
-	// Note the use of set_code_style to revert colours...
+    
+	// Set the style for line numbers. In order to get style for line numbers working, the <li> element
+    // is being styled. This means that the code on the line will also be styled, and most of the time
+    // you don't want this. So the set_code_style reverts styles for the line (by using a <div> on the line).
+    // So the source output looks like this:
+    //
+    // <pre style="[set_overall_style styles]"><ol>
+    // <li style="[set_line_style styles]"><div style="[set_code_style styles]>...</div></li>
+    // ...
+    // </ol></pre>
 	$geshi->set_line_style('font: normal normal 95% \'Courier New\', Courier, monospace; color: #003030;', 'font-weight: bold; color: #006060;', true);
 	$geshi->set_code_style('color: #000020;', 'color: #000020;');
 
+    // Styles for hyperlinks in the code. GESHI_LINK for default styles, GESHI_HOVER for hover style etc...
+    // note that classes must be enabled for this to work.
 	$geshi->set_link_styles(GESHI_LINK, 'color: #000060;');
 	$geshi->set_link_styles(GESHI_HOVER, 'background-color: #f0f000;');
 
+    // Use the header/footer functionality. This puts a div with content within the PRE element, so it is
+    // affected by the styles set by set_overall_style. So if the PRE has a border then the header/footer will
+    // appear inside it.
 	$geshi->set_header_content('GeSHi &copy; 2004, Nigel McNie. View source of example.php for example of using GeSHi');
 	$geshi->set_header_content_style('font-family: Verdana, Arial, sans-serif; color: #808080; font-size: 70%; font-weight: bold; background-color: #f0f0ff; border-bottom: 1px solid #d0d0d0; padding: 2px;');
 
+    // You can use <TIME> and <VERSION> as placeholders
 	$geshi->set_footer_content('Parsed in <TIME> seconds,  using GeSHi <VERSION>');
 	$geshi->set_footer_content_style('font-family: Verdana, Arial, sans-serif; color: #808080; font-size: 70%; font-weight: bold; background-color: #f0f0ff; border-top: 1px solid #d0d0d0; padding: 2px;');
 }
@@ -55,6 +94,7 @@ if ( isset($_POST['submit']) )
 	<?php
 	if ( isset($_POST['submit']) )
 	{
+        // Output the stylesheet. Note it doesn't output the <style> tag
 		echo $geshi->get_stylesheet();
 	}
 	?>
@@ -107,13 +147,14 @@ if ( isset($_POST['submit']) )
 </head>
 <body>
 <h2>GeSHi Example Script</h2>
-<p>To use this script, make sure that <strong>geshi.php</strong> is in the parent directory, and that
-the language files are in a subdirectory of the parent directory called <strong>geshi/</strong></p>
+<p>To use this script, make sure that <strong>geshi.php</strong> is in the parent directory or in your
+include_path, and that the language files are in a subdirectory of GeSHi's directory called <strong>geshi/</strong>.</p>
 <p>Enter your source and a language to highlight the source in and submit, or just choose a language to
 have that language file highlighted in PHP.</p>
 <?php
 if ( isset($_POST['submit']) )
 {
+    // The fun part :)
 	echo $geshi->parse_code();
 	echo '<hr />';
 }
