9d45ed6c3c1152163ddb620a6687bab6ff3a13a7
Nigel McNie <nigel@catalyst.net.nz>
2005-11-27 01:08:26 +0000

    Added support for backtick strings (Juan J. Martinez).

--- src/geshi/ruby.php
+++ src/geshi/ruby.php
@@ -46,7 +46,7 @@ $language_data = array (
 	'COMMENT_SINGLE' => array(1 => "#"),
 	'COMMENT_MULTI' => array(),
 	'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
-	'QUOTEMARKS' => array('"'),
+	'QUOTEMARKS' => array('"', '`'),
 	'ESCAPE_CHAR' => '\\',
 	'KEYWORDS' => array(
 		1 => array(
