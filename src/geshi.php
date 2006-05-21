cfe35f413e5b990451018c0c5a7cb2cfbefe2127
Nigel McNie <nigel@catalyst.net.nz>
2006-05-21 00:01:20 +0000

    Updated versions for 1.0.7.10 release.

--- src/geshi.php
+++ src/geshi.php
@@ -40,7 +40,7 @@
 //
 
 /** The version of this GeSHi file */
-define('GESHI_VERSION', '1.0.7.9');
+define('GESHI_VERSION', '1.0.7.10');
 
 /** Set the correct directory separator */
 define('GESHI_DIR_SEPARATOR', ('WIN' != substr(PHP_OS, 0, 3)) ? '/' : '\\');
