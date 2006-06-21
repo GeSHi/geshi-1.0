0fbea1ad43c76e407a6b3d5dce13d21467f84084
Nigel McNie <nigel@catalyst.net.nz>
2006-06-21 10:22:51 +0000

    Moved a couple of functions to the correct group in smarty (arwan).

--- src/geshi/smarty.php
+++ src/geshi/smarty.php
@@ -50,7 +50,7 @@ $language_data = array (
 		1 => array(
 			'$smarty', 'now', 'const', 'capture', 'config', 'section', 'foreach', 'template', 'version', 'ldelim', 'rdelim',
 			'config_load', 'foreachelse', 'include', 'include_php', 'insert', 'if', 'elseif', 'else', 'php',
-			'sectionelse', 'clear_all_cache', 'clear_cache', 'is_cached', 'from', 'item'
+			'sectionelse', 'clear_all_cache', 'clear_cache', 'is_cached',
 			),
 		2 => array(
 			'capitalize', 'count_characters', 'cat', 'count_paragraphs', 'count_sentences', 'count_words', 'date_format',
@@ -83,7 +83,7 @@ $language_data = array (
 			),
 		6 => array(
 			'name', 'assign', 'file', 'scope', 'global', 'key', 'once', 'script',
-			'loop', 'start', 'step', 'max', 'show', 'values', 'value'
+			'loop', 'start', 'step', 'max', 'show', 'values', 'value', 'from', 'item'
 			),
 		7 => array(
 			'eq', 'neq', 'ne', 'lte', 'gte', 'ge', 'le', 'not', 'mod'
