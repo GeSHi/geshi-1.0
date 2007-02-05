78d3cd597f415ab6c786bd6bcdac298522dee915
Nigel McNie <nigel@catalyst.net.nz>
2007-02-05 21:13:06 +0000

    Merged r898 to trunk

--- src/geshi/python.php
+++ src/geshi/python.php
@@ -138,7 +138,7 @@ $language_data = array (
 			'__iter__', 'next',
 			// String types: http://python.org/doc/current/lib/string-methods.html
 			'capitalize', 'center', 'count', 'decode', 'encode', 'endswith', 'expandtabs',
-			'find', 'index', 'isalnum', 'isaplpha', 'isdigit', 'islower', 'isspace', 'istitle',
+			'find', 'index', 'isalnum', 'isalpha', 'isdigit', 'islower', 'isspace', 'istitle',
 			'isupper', 'join', 'ljust', 'lower', 'lstrip', 'replace', 'rfind', 'rindex', 'rjust',
 			'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title',
 			'translate', 'upper', 'zfill',
