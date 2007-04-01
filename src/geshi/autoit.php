baf23f6d562aa8446b77518aff9e400a45f26794
Nigel McNie <nigel@catalyst.net.nz>
2007-04-01 12:05:13 +0000

    Bumped version for 1.0.7.19 release

--- src/geshi/autoit.php
+++ src/geshi/autoit.php
@@ -4,7 +4,7 @@
  * --------
  * Author: mastrboy
  * Copyright: (c) 2006 and to GESHi ;)
- * Release Version: 1.0.7.18
+ * Release Version: 1.0.7.19
  * Date Started: 26.01.2006
  *
  * Current bugs & todo:
