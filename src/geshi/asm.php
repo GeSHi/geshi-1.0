2778358692b8feba7b53276796c869b9f43429de
Nigel McNie <nigel@catalyst.net.nz>
2007-04-01 10:38:04 +0000

    Changed some regular expressions to possibly help with badly performing regex support in PHP (Tim Starling)

--- src/geshi/asm.php
+++ src/geshi/asm.php
@@ -185,7 +185,7 @@ $language_data = array (
 	'OBJECT_SPLITTERS' => array(
 		),
 	'REGEXPS' => array(
-		0 => '0[0-9a-fA-F][0-9a-fA-F]*[hH]',
+		0 => '0[0-9a-fA-F]{1,32}[hH]',
 		1 => '[01][01]*[bB]'
 		),
 	'STRICT_MODE_APPLIES' => GESHI_NEVER,
