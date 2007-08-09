45486521028976c07288cad2c67f0c513ae5b693
Nigel McNie <nigel@catalyst.net.nz>
2007-08-09 09:36:20 +0000

    Backported lua string fix to stable

--- src/geshi/lua.php
+++ src/geshi/lua.php
@@ -47,7 +47,7 @@ $language_data = array (
 	'COMMENT_SINGLE' => array(1 => "--"),
 	'COMMENT_MULTI' => array('--[[' => ']]'),
 	'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
-	'QUOTEMARKS' => array('"'),
+	'QUOTEMARKS' => array("'", '"'),
 	'ESCAPE_CHAR' => '\\',
 	'KEYWORDS' => array(
 		1 => array(
