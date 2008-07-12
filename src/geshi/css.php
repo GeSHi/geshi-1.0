7914252020a9d208127a895ccd8a1f149cb9c6fc
Milian Wolff <mail@milianw.de>
2008-07-12 14:57:58 +0000

    chg: add missing symbols and use a darker green for symbols, making it more readable

--- src/geshi/css.php
+++ src/geshi/css.php
@@ -54,9 +54,6 @@ $language_data = array (
     'LANG_NAME' => 'CSS',
     'COMMENT_SINGLE' => array(1 => '@'),
     'COMMENT_MULTI' => array('/*' => '*/'),
-    'COMMENT_REGEXP' => array(
-        2 => "/(?<=\\()\\s*(?:(?:[a-z0-9]+?:\\/\\/)?[a-z0-9_\\-\\.\\/:]+?)?[a-z]+?\\.[a-z]+?(\\?[^\)]+?)?\\s*?(?=\\))/i"
-        ),
     'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
     'QUOTEMARKS' => array('"', "'"),
     'ESCAPE_CHAR' => '\\',
@@ -135,7 +132,8 @@ $language_data = array (
             )
         ),
     'SYMBOLS' => array(
-        '(', ')', '{', '}', ':', ';'
+        '(', ')', '{', '}', ':', ';',
+        '>', '+', '*', ',', '^', '='
         ),
     'CASE_SENSITIVE' => array(
         GESHI_COMMENTS => false,
@@ -149,14 +147,13 @@ $language_data = array (
             ),
         'COMMENTS' => array(
             1 => 'color: #a1a100;',
-            2 => 'color: #ff0000; font-style: italic;',
             'MULTI' => 'color: #808080; font-style: italic;'
             ),
         'ESCAPE_CHAR' => array(
             0 => 'color: #000099; font-weight: bold;'
             ),
         'BRACKETS' => array(
-            0 => 'color: #66cc66;'
+            0 => 'color: #00AA00;'
             ),
         'STRINGS' => array(
             0 => 'color: #ff0000;'
@@ -167,7 +164,7 @@ $language_data = array (
         'METHODS' => array(
             ),
         'SYMBOLS' => array(
-            0 => 'color: #66cc66;'
+            0 => 'color: #00AA00;'
             ),
         'SCRIPT' => array(
             ),
