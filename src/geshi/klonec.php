4f5b69e66e588efa7157376f488f0da9127aa01b
Milian Wolff <mail@milianw.de>
2008-07-21 21:40:26 +0000

    fix: prevent notices

--- src/geshi/klonec.php
+++ src/geshi/klonec.php
@@ -222,6 +222,10 @@ $language_data = array (
         1 => false,
         2 => false,
         3 => false,
+        4 => false,
+        5 => false,
+        6 => false,
+        7 => false
         ),
     'STYLES' => array(
         'KEYWORDS' => array(
