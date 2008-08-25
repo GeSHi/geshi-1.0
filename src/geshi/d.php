9c6fe6b909be8f3a29ee833606bd85affd36c46b
Benny Baumann <benbe@geshi.org>
2008-08-25 00:22:42 +0000

    add: Improved Escape Char support for D according to documentation at http://digitalmars.com/d/1.0/lex.html#StringLiteral

--- src/geshi/d.php
+++ src/geshi/d.php
@@ -56,7 +56,43 @@ $language_data = array (
         ),
     'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
     'QUOTEMARKS' => array('"', "'"),
-    'ESCAPE_CHAR' => '\\',
+    'ESCAPE_CHAR' => '',
+    'ESCAPE_REGEXP' => array(
+        //Simple Single Char Escapes
+        1 => "#\\\\[abfnrtv\\'\"?\n]#i",
+        //Hexadecimal Char Specs
+        2 => "#\\\\x[\da-fA-F]{2}#",
+        //Hexadecimal Char Specs
+        3 => "#\\\\u[\da-fA-F]{4}#",
+        //Hexadecimal Char Specs
+        4 => "#\\\\U[\da-fA-F]{8}#",
+        //Octal Char Specs
+        5 => "#\\\\[0-7]{1,3}#",
+        //Named entity escapes
+        6 => "#\\\\&(?:quot|amp|lt|gt|OElig|oelig|Scaron|scaron|Yuml|circ|tilde|".
+            "ensp|emsp|thinsp|zwnj|zwj|lrm|rlm|ndash|mdash|lsquo|rsquo|sbquo|".
+            "ldquo|rdquo|bdquo|dagger|Dagger|permil|lsaquo|rsaquo|euro|nbsp|".
+            "iexcl|cent|pound|curren|yen|brvbar|sect|uml|copy|ordf|laquo|not|".
+            "shy|reg|macr|deg|plusmn|sup2|sup3|acute|micro|para|middot|cedil|".
+            "sup1|ordm|raquo|frac14|frac12|frac34|iquest|Agrave|Aacute|Acirc|".
+            "Atilde|Auml|Aring|AElig|Ccedil|Egrave|Eacute|Ecirc|Euml|Igrave|".
+            "Iacute|Icirc|Iuml|ETH|Ntilde|Ograve|Oacute|Ocirc|Otilde|Ouml|".
+            "times|Oslash|Ugrave|Uacute|Ucirc|Uuml|Yacute|THORN|szlig|agrave|".
+            "aacute|acirc|atilde|auml|aring|aelig|ccedil|egrave|eacute|ecirc|".
+            "euml|igrave|iacute|icirc|iuml|eth|ntilde|ograve|oacute|ocirc|".
+            "otilde|ouml|divide|oslash|ugrave|uacute|ucirc|uuml|yacute|thorn|".
+            "yuml|fnof|Alpha|Beta|Gamma|Delta|Epsilon|Zeta|Eta|Theta|Iota|".
+            "Kappa|Lambda|Mu|Nu|Xi|Omicron|Pi|Rho|Sigma|Tau|Upsilon|Phi|Chi|".
+            "Psi|Omega|alpha|beta|gamma|delta|epsilon|zeta|eta|theta|iota|".
+            "kappa|lambda|mu|nu|xi|omicron|pi|rho|sigmaf|sigma|tau|upsilon|".
+            "phi|chi|psi|omega|thetasym|upsih|piv|bull|hellip|prime|Prime|".
+            "oline|frasl|weierp|image|real|trade|alefsym|larr|uarr|rarr|darr|".
+            "harr|crarr|lArr|uArr|rArr|dArr|hArr|forall|part|exist|empty|".
+            "nabla|isin|notin|ni|prod|sum|minus|lowast|radic|prop|infin|ang|".
+            "and|or|cap|cup|int|there4|sim|cong|asymp|ne|equiv|le|ge|sub|sup|".
+            "nsub|sube|supe|oplus|otimes|perp|sdot|lceil|rceil|lfloor|rfloor|".
+            "lang|rang|loz|spades|clubs|hearts|diams);#",
+        ),
     'HARDQUOTE' => array('`', '`'),
     'HARDESCAPE' => array(),
     'NUMBERS' =>
@@ -154,6 +190,12 @@ $language_data = array (
             ),
         'ESCAPE_CHAR' => array(
             0 => 'color: #000099; font-weight: bold;',
+            1 => 'color: #000099; font-weight: bold;',
+            2 => 'color: #660099; font-weight: bold;',
+            3 => 'color: #660099; font-weight: bold;',
+            4 => 'color: #660099; font-weight: bold;',
+            5 => 'color: #006699; font-weight: bold;',
+            6 => 'color: #666699; font-weight: bold; font-style: italic;',
             'HARD' => '',
             ),
         'BRACKETS' => array(
