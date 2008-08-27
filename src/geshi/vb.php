781fbbc6fb74945b3ad7e8a60ac846a98de21745
Milian Wolff <mail@milianw.de>
2008-08-27 19:20:17 +0000

    chg: use a different keyword list for VB with better Visual Studio compliance. Thanks to Claus von der Burchard for the report

--- src/geshi/vb.php
+++ src/geshi/vb.php
@@ -3,7 +3,8 @@
  * vb.php
  * ------
  * Author: Roberto Rossi (rsoftware@altervista.org)
- * Copyright: (c) 2004 Roberto Rossi (http://rsoftware.altervista.org), Nigel McNie (http://qbnz.com/highlighter)
+ * Copyright: (c) 2004 Roberto Rossi (http://rsoftware.altervista.org),
+ *                     Nigel McNie (http://qbnz.com/highlighter)
  * Release Version: 1.0.8
  * Date Started: 2004/08/30
  *
@@ -11,6 +12,10 @@
  *
  * CHANGES
  * -------
+ * 2008/08/27 (1.0.8.1)
+ *  -  changed keyword list for better Visual Studio compliance
+ * 2008/08/26 (1.0.8.1)
+ *  -  Fixed multiline comments
  * 2004/11/27 (1.0.1)
  *  -  Added support for multiple object splitters
  * 2004/08/30 (1.0.0)
@@ -52,52 +57,23 @@ $language_data = array (
     'ESCAPE_CHAR' => '',
     'KEYWORDS' => array(
         1 => array(
-            'as', 'err', 'boolean', 'and', 'or', 'recordset', 'unload', 'to',
-            'integer','long','single','new','database','nothing','set','close',
-            'open','print','split','line','field','querydef','instrrev',
-            'abs','array','asc','ascb','ascw','atn','avg','me',
-            'cbool','cbyte','ccur','cdate','cdbl','cdec','choose','chr','chrb','chrw','cint','clng',
-            'command','cos','count','createobject','csng','cstr','curdir','cvar','cvdate','cverr',
-            'date','dateadd','datediff','datepart','dateserial','datevalue','day','ddb','dir','doevents',
-            'environ','eof','error','exp',
-            'fileattr','filedatetime','filelen','fix','format','freefile','fv',
-            'getallstrings','getattr','getautoserversettings','getobject','getsetting',
-            'hex','hour','iif','imestatus','input','inputb','inputbox','instr','instb','int','ipmt',
-            'isarray','isdate','isempty','iserror','ismissing','isnull','isnumeric','isobject',
-            'lbound','lcase','left','leftb','len','lenb','loadpicture','loc','lof','log','ltrim',
-            'max','mid','midb','min','minute','mirr','month','msgbox',
-            'now','nper','npv','oct','partition','pmt','ppmt','pv','qbcolor',
-            'rate','rgb','right','rightb','rnd','rtrim',
-            'second','seek','sgn','shell','sin','sln','space','spc','sqr','stdev','stdevp','str',
-            'strcomp','strconv','string','switch','sum','syd',
-            'tab','tan','time','timer','timeserial','timevalue','trim','typename',
-            'ubound','ucase','val','var','varp','vartype','weekday','year',
-            'appactivate','base','beep','call','case','chdir','chdrive','const',
-            'declare','defbool','defbyte','defcur','defdate','defdbl','defdec','defint',
-            'deflng','defobj','defsng','defstr','deftype','defvar','deletesetting','dim','do',
-            'else','elseif','end','enum','erase','event','exit','explicit',
-            'false','filecopy','for','foreach','friend','function','get','gosub','goto',
-            'if','implements','kill','let','lineinput','lock','loop','lset','mkdir','name','next','not',
-            'onerror','on','option','private','property','public','put','raiseevent','randomize',
-            'redim','rem','reset','resume','return','rmdir','rset',
-            'savepicture','savesetting','sendkeys','setattr','static','sub',
-            'then','true','type','unlock','wend','while','width','with','write',
-            'vbabort','vbabortretryignore','vbapplicationmodal','vbarray',
-            'vbbinarycompare','vbblack','vbblue','vbboolean','vbbyte','vbcancel',
-            'vbcr','vbcritical','vbcrlf','vbcurrency','vbcyan','vbdataobject',
-            'vbdate','vbdecimal','vbdefaultbutton1','vbdefaultbutton2',
-            'vbdefaultbutton3','vbdefaultbutton4','vbdouble','vbempty',
-            'vberror','vbexclamation','vbfirstfourdays','vbfirstfullweek',
-            'vbfirstjan1','vbformfeed','vbfriday','vbgeneraldate','vbgreen',
-            'vbignore','vbinformation','vbinteger','vblf','vblong','vblongdate',
-            'vblongtime','vbmagenta','vbmonday','vbnewline','vbno','vbnull',
-            'vbnullchar','vbnullstring','vbobject','vbobjecterror','vbok','vbokcancel',
-            'vbokonly','vbquestion','vbred','vbretry','vbretrycancel','vbsaturday',
-            'vbshortdate','vbshorttime','vbsingle','vbstring','vbsunday',
-            'vbsystemmodal','vbtab','vbtextcompare','vbthursday','vbtuesday',
-            'vbusesystem','vbusesystemdayofweek','vbvariant','vbverticaltab',
-            'vbwednesday','vbwhite','vbyellow','vbyes','vbyesno','vbyesnocancel',
-            'vbnormal','vbdirectory'
+            'AddressOf', 'Alias', 'And', 'Append', 'As', 'BF', 'Binary',
+            'Boolean', 'ByRef', 'Byte', 'ByVal', 'Call', 'Case', 'CBool',
+            'CByte', 'CCur', 'CDate', 'CDbl', 'CDec', 'CInt', 'CLng',
+            'Close', 'Collection', 'Const', 'Control', 'CSng', 'CStr',
+            'Currency', 'CVar', 'Date', 'Declare', 'Dim', 'Do', 'Double',
+            'Each', 'Else', 'ElseIf', 'End', 'Enum', 'Erase', 'Error',
+            'Event', 'Exit', 'Explicit', 'False', 'For', 'Friend',
+            'Function', 'Get', 'GoSub', 'Goto', 'If', 'Implements', 'In',
+            'Input', 'Integer', 'Is', 'LBound', 'Let', 'Lib', 'Like',
+            'Line', 'Long', 'Loop', 'Mod', 'New', 'Next', 'Not',
+            'Nothing', 'Object', 'On', 'Open', 'Option', 'Optional',
+            'Or', 'Output', 'ParamArray', 'Preserve', 'Print', 'Private',
+            'Property', 'Public', 'RaiseEvent', 'Random', 'ReDim',
+            'Resume', 'Select', 'Set', 'Single', 'Static', 'Step',
+            'Stop', 'String', 'Sub', 'Then', 'To', 'True', 'Type',
+            'TypeOf', 'UBound', 'Until', 'Variant', 'While', 'With',
+            'WithEvents', 'Xor'
             )
         ),
     'SYMBOLS' => array(
