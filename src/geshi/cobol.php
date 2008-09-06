cb117077ff0c32cbc8d1ec86ee38845bfaea4cce
Benny Baumann <benbe@geshi.org>
2008-09-06 18:06:35 +0000

    fix: Some more reformatting fix: Floats for COBOL fixed

--- src/geshi/cobol.php
+++ src/geshi/cobol.php
@@ -43,6 +43,11 @@ $language_data = array (
     'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
     'QUOTEMARKS' => array('"', "'"),
     'ESCAPE_CHAR' => '\\',
+    'NUMBERS' =>
+        GESHI_NUMBER_INT_BASIC |
+        GESHI_NUMBER_FLT_NONSCI |
+        GESHI_NUMBER_FLT_SCI_SHORT |
+        GESHI_NUMBER_FLT_SCI_ZERO,
     'KEYWORDS' => array(
         1 => array( //Compiler Directives
             "ANSI", "BLANK", "NOBLANK", "CALL-SHARED", "CANCEL", "NOCANCEL",
