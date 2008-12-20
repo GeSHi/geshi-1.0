3929d11f351b826dae2dd10e98032078b03d5dec
Benny Baumann <benbe@geshi.org>
2008-12-20 12:40:11 +0000

    fix: Optimized LScript language file to use PARSER_CONTROL in KW groups 3 and 4

--- src/geshi/lscript.php
+++ src/geshi/lscript.php
@@ -32,7 +32,7 @@
 $language_data = array (
     'LANG_NAME' => 'LScript',
     'COMMENT_SINGLE' => array(1 => '//'),
-    'COMMENT_MULTI' => array(),
+    'COMMENT_MULTI' => array('/*' => '*/'),
     'CASE_KEYWORDS' => GESHI_CAPS_NO_CHANGE,
     'QUOTEMARKS' => array('"'),
     'ESCAPE_CHAR' => '',
@@ -49,82 +49,82 @@ $language_data = array (
             'foreach', 'if', 'return', 'switch', 'true', 'while',
             ),
         3 => array(
-            '.active', '.alertlevel', '.alpha', '.alphaprefix', '.animfilename', '.autokeycreate',
-            '.backdroptype', '.blue', '.boxthreshold', '.button',
-            '.channelsvisible', '.childrenvisible', '.compfg', '.compbg', '.compfgalpha',
-            '.coneangles', '.cosine', '.count', '.ctl', '.curFilename', '.curFrame',
-            '.currenttime', '.curTime', '.curType',
-            '.depth', '.diffshade', '.diffuse', '.dimensions', '.displayopts', '.dynamicupdate',
-            '.end', '.eta',
-            '.filename', '.flags', '.fogtype', '.fps', '.frame', '.frameend', '.frameheight',
-            '.framestart', '.framestep', '.framewidth',
-            '.generalopts', '.genus', '.geometry', '.gNorm', '.goal', '.green',
-            '.h', '.hasAlpha', '.height',
-            '.id', '.innerlimit', '.isColor',
-            '.keyCount', '.keys',
-            '.limiteregion', '.locked', '.luminous',
-            '.maxsamplesperpixel', '.minsamplesperpixel', '.mirror', '.motionx', '.motiony',
-            '.name', '.newFilename', '.newFrame', '.newTime', '.newType', '.null', '.numthreads',
-            '.objID', '.oPos', '.outerlimit', 'oXfrm',
-            '.parent', '.pixel', '.pixelaspect', '.point', '.points', '.pointcount', '.polNum',
-            '.polycount', '.polygon', '.polygons', '.postBehavior', '.preBehavior', '.previewend',
-            '.previewstart', '.previewstep',
-            '.range', '.rawblue', '.rawgreen', '.rawred', '.rayLength', '.raySource', '.red',
-            '.reflectblue', '.reflectgreen', '.reflectred', '.recursiondepth', '.renderend',
-            '.renderopts', '.renderstart', '.renderstep', '.rendertype', '.restlength',
-            '.rgbprefix', '.roughness',
-            '.selected', '.setColor', '.setPattern', '.shading', '.shadow', '.shadows',
-            '.shadowtype', '.size', '.source', '.special', '.specshade', '.specular',
-            '.spotsize', '.start', '.sx', '.sy', '.sz',
-            '.target', '.totallayers', '.totalpoints', '.totalpolygons', '.trans', '.transparency',
-            '.type',
-            '.value', '.view', '.visible', '.visibility',
-            '.w', '.width', '.wNorm', '.wPos', '.wXfrm',
-            '.x', '.xoffset',
-            '.y', '.yoffset',
-            '.z'
+            'active', 'alertlevel', 'alpha', 'alphaprefix', 'animfilename', 'autokeycreate',
+            'backdroptype', 'blue', 'boxthreshold', 'button',
+            'channelsvisible', 'childrenvisible', 'compfg', 'compbg', 'compfgalpha',
+            'coneangles', 'cosine', 'count', 'ctl', 'curFilename', 'curFrame',
+            'currenttime', 'curTime', 'curType',
+            'depth', 'diffshade', 'diffuse', 'dimensions', 'displayopts', 'dynamicupdate',
+            'end', 'eta',
+            'filename', 'flags', 'fogtype', 'fps', 'frame', 'frameend', 'frameheight',
+            'framestart', 'framestep', 'framewidth',
+            'generalopts', 'genus', 'geometry', 'gNorm', 'goal', 'green',
+            'h', 'hasAlpha', 'height',
+            'id', 'innerlimit', 'isColor',
+            'keyCount', 'keys',
+            'limiteregion', 'locked', 'luminous',
+            'maxsamplesperpixel', 'minsamplesperpixel', 'mirror', 'motionx', 'motiony',
+            'name', 'newFilename', 'newFrame', 'newTime', 'newType', 'null', 'numthreads',
+            'objID', 'oPos', 'outerlimit', 'oXfrm',
+            'parent', 'pixel', 'pixelaspect', 'point', 'points', 'pointcount', 'polNum',
+            'polycount', 'polygon', 'polygons', 'postBehavior', 'preBehavior', 'previewend',
+            'previewstart', 'previewstep',
+            'range', 'rawblue', 'rawgreen', 'rawred', 'rayLength', 'raySource', 'red',
+            'reflectblue', 'reflectgreen', 'reflectred', 'recursiondepth', 'renderend',
+            'renderopts', 'renderstart', 'renderstep', 'rendertype', 'restlength',
+            'rgbprefix', 'roughness',
+            'selected', 'setColor', 'setPattern', 'shading', 'shadow', 'shadows',
+            'shadowtype', 'size', 'source', 'special', 'specshade', 'specular',
+            'spotsize', 'start', 'sx', 'sy', 'sz',
+            'target', 'totallayers', 'totalpoints', 'totalpolygons', 'trans', 'transparency',
+            'type',
+            'value', 'view', 'visible', 'visibility',
+            'w', 'width', 'wNorm', 'wPos', 'wXfrm',
+            'x', 'xoffset',
+            'y', 'yoffset',
+            'z'
             ),
         4 => array(
-            '.addLayer', '.addParticle', '.alphaspot', '.ambient', '.asAsc', '.asBin',
-            '.asInt', '.asNum', '.asStr', '.asVec', '.attach', '.axislocks',
-            '.backdropColor', '.backdropRay', '.backdropSqueeze', '.bone', '.blurLength',
-            '.close', '.color', '.contains', '.copy', '.createKey',
-            '.deleteKey', '.detach', '.drawCircle', '.drawLine', '.drawPoint', '.drawText',
-            '.drawTriangle',
-            '.edit', '.eof', '.event',
-            '.firstChannel', '.firstLayer', '.firstSelect', '.focalLength', '.fogColor',
-            '.fogMaxAmount', '.fogMaxDist', '.fogMinAmount', '.fogMinDist',
-            '.fovAngles', '.fStop', '.firstChild', '.focalDistance',
-            '.get', '.getChannelGroup', '.getEnvelope', '.getForward', '.getKeyBias',
-            '.getKeyContinuity', '.getKeyCurve', '.getKeyHermite', '.getKeyTension',
-            '.getKeyTime', '.getKeyValue', '.getParticle', '.getPivot', '.getPosition',
-            '.getRight', '.getRotation', '.getSelect', '.getScaling', '.getTag', '.getTexture',
-            '.getUp', '.getValue', '.getWorldPosition', '.getWorldForward', '.getWorldRight',
-            '.getWorldRotation', '.getWorldUp', '.globalBlur', '.globalMask', '.globalResolution',
-            '.hasCCEnd', '.hasCCStart',
-            '.illuminate', '.indexOf', '.isAscii', '.isAlnum', '.isAlpha', '.isBone',
-            '.isCamera', '.isChannel', '.isChannelGroup', '.isCntrl', '.isCurve', '.isDigit',
-            '.isEnvelope', '.isImage', '.isInt', '.isLight', '.isLower', '.isMapped', '.isMesh',
-            '.isNil', '.isNum', '.IsOpen', '.isOriginal', '.isPrint', '.isPunct', '.isScene',
-            '.isSpace', '.isStr', '.isUpper', '.isValid', '.isVMap', '.isVec', '.isXDigit',
-            '.keyExists',
-            '.layer', '.layerName', '.layerVisible', '.limits', '.line', '.linecount', '.load', '.luma',
-            '.next', '.nextLayer', '.nextSelect', '.nextChannel', '.nextChild', '.nl',
-            '.offset', '.open',
-            '.pack', '.param', '.parse', '.paste', '.persist', '.polygonCount', '.position',
-            '.rayCast', '.rayTrace', '.read', '.readByte', '.readInt', '.readNumber',
-            '.readDouble', '.readShort', '.readString', '.readVector', '.reduce',
-            '.remParticle', '.renderCamera', '.reopen', '.replace', '.reset', '.restParam',
-            '.rewind', '.rgb', '.rgbambient', '.rgbcolor', '.rgbspot',
-            '.save', '.schemaPosition', '.select', '.set', '.setChannelGroup', '.setKeyBias',
-            '.setKeyContinuity', '.setKeyCurve',
-            '.setKeyHermite', '.setKeyTension', '.setKeyValue', '.setParticle', '.setPoints',
-            '.setTag', '.setValue', '.server', '.serverFlags', '.sortA', '.sortD', '.surface',
-            '.trunc',
-            '.write', '.writeln', '.writeByte', '.writeData', '.writeNumber', '.writeDouble',
-            '.writeShort', '.writeString', '.writeVector',
-            '.vertex', '.vertexCount',
-            '.zoomFactor'
+            'addLayer', 'addParticle', 'alphaspot', 'ambient', 'asAsc', 'asBin',
+            'asInt', 'asNum', 'asStr', 'asVec', 'attach', 'axislocks',
+            'backdropColor', 'backdropRay', 'backdropSqueeze', 'bone', 'blurLength',
+            'close', 'color', 'contains', 'copy', 'createKey',
+            'deleteKey', 'detach', 'drawCircle', 'drawLine', 'drawPoint', 'drawText',
+            'drawTriangle',
+            'edit', 'eof', 'event',
+            'firstChannel', 'firstLayer', 'firstSelect', 'focalLength', 'fogColor',
+            'fogMaxAmount', 'fogMaxDist', 'fogMinAmount', 'fogMinDist',
+            'fovAngles', 'fStop', 'firstChild', 'focalDistance',
+            'get', 'getChannelGroup', 'getEnvelope', 'getForward', 'getKeyBias',
+            'getKeyContinuity', 'getKeyCurve', 'getKeyHermite', 'getKeyTension',
+            'getKeyTime', 'getKeyValue', 'getParticle', 'getPivot', 'getPosition',
+            'getRight', 'getRotation', 'getSelect', 'getScaling', 'getTag', 'getTexture',
+            'getUp', 'getValue', 'getWorldPosition', 'getWorldForward', 'getWorldRight',
+            'getWorldRotation', 'getWorldUp', 'globalBlur', 'globalMask', 'globalResolution',
+            'hasCCEnd', 'hasCCStart',
+            'illuminate', 'indexOf', 'isAscii', 'isAlnum', 'isAlpha', 'isBone',
+            'isCamera', 'isChannel', 'isChannelGroup', 'isCntrl', 'isCurve', 'isDigit',
+            'isEnvelope', 'isImage', 'isInt', 'isLight', 'isLower', 'isMapped', 'isMesh',
+            'isNil', 'isNum', 'IsOpen', 'isOriginal', 'isPrint', 'isPunct', 'isScene',
+            'isSpace', 'isStr', 'isUpper', 'isValid', 'isVMap', 'isVec', 'isXDigit',
+            'keyExists',
+            'layer', 'layerName', 'layerVisible', 'limits', 'line', 'linecount', 'load', 'luma',
+            'next', 'nextLayer', 'nextSelect', 'nextChannel', 'nextChild', 'nl',
+            'offset', 'open',
+            'pack', 'param', 'parse', 'paste', 'persist', 'polygonCount', 'position',
+            'rayCast', 'rayTrace', 'read', 'readByte', 'readInt', 'readNumber',
+            'readDouble', 'readShort', 'readString', 'readVector', 'reduce',
+            'remParticle', 'renderCamera', 'reopen', 'replace', 'reset', 'restParam',
+            'rewind', 'rgb', 'rgbambient', 'rgbcolor', 'rgbspot',
+            'save', 'schemaPosition', 'select', 'set', 'setChannelGroup', 'setKeyBias',
+            'setKeyContinuity', 'setKeyCurve',
+            'setKeyHermite', 'setKeyTension', 'setKeyValue', 'setParticle', 'setPoints',
+            'setTag', 'setValue', 'server', 'serverFlags', 'sortA', 'sortD', 'surface',
+            'trunc',
+            'write', 'writeln', 'writeByte', 'writeData', 'writeNumber', 'writeDouble',
+            'writeShort', 'writeString', 'writeVector',
+            'vertex', 'vertexCount',
+            'zoomFactor'
             ),
         2 => array(
             'abs', 'acos', 'angle', 'append', 'ascii', 'asin', 'atan',
@@ -305,7 +305,7 @@ $language_data = array (
             ),
         ),
     'SYMBOLS' => array(
-        '(', ')', '{', '}', '=', '+', '-', '*', '/', '!', '%', '&', '@'
+        '(', ')', '{', '}', '[', ']', '=', '<', '>', '+', '-', '*', '/', '!', '%', '&', '@'
         ),
     'CASE_SENSITIVE' => array(
         GESHI_COMMENTS => true,
@@ -329,9 +329,10 @@ $language_data = array (
             ),
         'COMMENTS' => array(
             1 => 'color: #7F7F7F;',
+			'MULTI' => 'color: #7F7F7F;'
             ),
         'BRACKETS' => array(
-            0 => 'color: #000000;'
+            0 => 'color: #0040A0;'
             ),
         'STRINGS' => array(
             0 => 'color: #00C800;'
@@ -342,7 +343,7 @@ $language_data = array (
         'METHODS' => array(
             ),
         'SYMBOLS' => array(
-            0 => 'color: #000000;'
+            0 => 'color: #0040A0;'
             ),
         'REGEXPS' => array(
             ),
@@ -370,7 +371,17 @@ $language_data = array (
         ),
     'HIGHLIGHT_STRICT_BLOCK' => array(
         ),
-    'TAB_WIDTH' => 4
+    'TAB_WIDTH' => 4,
+    'PARSER_CONTROL' => array(
+        'KEYWORDS' => array(
+            3 => array(
+                'DISALLOWED_BEFORE' => '(?<=\.)'
+                ),
+            4 => array(
+                'DISALLOWED_BEFORE' => '(?<=\.)'
+                )
+            )
+        )
 );
 
 ?>
