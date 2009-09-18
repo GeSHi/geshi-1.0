e4e22791fee9dde6db14ac33c47b03d585b32745
Benny Baumann <benbe@geshi.org>
2009-09-18 15:49:42 +0000

    add: Missed throw in PHP language file

--- src/geshi/php.php
+++ src/geshi/php.php
@@ -90,7 +90,7 @@ $language_data = array(
             'as','break','case','continue','default','do','else','elseif',
             'endfor','endforeach','endif','endswitch','endwhile','for',
             'foreach','if','include','include_once','require','require_once',
-            'return','switch','while',
+            'return','switch','throw','while',
 
             'echo','print'
             ),
