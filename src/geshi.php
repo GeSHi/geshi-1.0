7f4f255a2b1b559d52a5acc0efb99c8add0883a8
Benny Baumann <benbe@geshi.org>
2010-01-06 13:41:21 +0000

    chg: Changes INT_BASIC number style to allow for .. before numbers to better support the range operator often to be found in Pascal-like languages

--- src/geshi.php
+++ src/geshi.php
@@ -1971,7 +1971,7 @@ class GeSHi {
             //All this formats are matched case-insensitively!
             static $numbers_format = array(
                 GESHI_NUMBER_INT_BASIC =>
-                    '(?<![0-9a-z_\.%])(?<![\d\.]e[+\-])([1-9]\d*?|0)(?![0-9a-z]|\.(?:[eE][+\-]?)?\d)',
+                    '(?:(?<![0-9a-z_\.%])|(?<=..))(?<![\d\.]e[+\-])([1-9]\d*?|0)(?![0-9a-z]|\.(?:[eE][+\-]?)?\d)',
                 GESHI_NUMBER_INT_CSTYLE =>
                     '(?<![0-9a-z_\.%])(?<![\d\.]e[+\-])([1-9]\d*?|0)l(?![0-9a-z]|\.(?:[eE][+\-]?)?\d)',
                 GESHI_NUMBER_BIN_SUFFIX =>
