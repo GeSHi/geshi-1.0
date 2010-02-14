805c1bbaaed014c43d54589ebd079a62f7c49eb3
Benny Baumann <benbe@geshi.org>
2010-02-14 19:51:34 +0000

    ver: Version Bump for 1.0.8.7

--- src/geshi/autoconf.php
+++ src/geshi/autoconf.php
@@ -1,33 +1,33 @@
 <?php
 /*************************************************************************************
-* autoconf.php
-* -----
-* Author: Mihai Vasilian (grayasm@gmail.com)
-* Copyright: (c) 2010 Mihai Vasilian
-* Release Version: 1.0.8.7
-* Date Started: 2010/01/25
-*
-* autoconf language file for GeSHi.
-*
-***********************************************************************************
-*
-*     This file is part of GeSHi.
-*
-*   GeSHi is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 2 of the License, or
-*   (at your option) any later version.
-*
-*   GeSHi is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with GeSHi; if not, write to the Free Software
-*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-************************************************************************************/
+ * autoconf.php
+ * -----
+ * Author: Mihai Vasilian (grayasm@gmail.com)
+ * Copyright: (c) 2010 Mihai Vasilian
+ * Release Version: 1.0.8.7
+ * Date Started: 2010/01/25
+ *
+ * autoconf language file for GeSHi.
+ *
+ ***********************************************************************************
+ *
+ *     This file is part of GeSHi.
+ *
+ *   GeSHi is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   GeSHi is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with GeSHi; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ************************************************************************************/
 
 $language_data = array (
     'LANG_NAME' => 'Autoconf',
