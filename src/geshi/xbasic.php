3134e7803e104d971a1eb88232cc07db667f6f98
Benny Baumann <benbe@geshi.org>
2010-07-22 01:17:09 +0000

    ver: Version Bump for 1.0.8.9

--- src/geshi/xbasic.php
+++ src/geshi/xbasic.php
@@ -4,7 +4,7 @@
  * ----------
  * Author: José Gabriel Moya Yangüela (josemoya@gmail.com)
  * Copyright: (c) 2005 José Gabriel Moya Yangüela (http://aprenderadesaprender.6te.net)
- * Release Version: 1.0.8.8
+ * Release Version: 1.0.8.9
  * Date Started: 2005/11/23
  * Last Modified: $Date: 2010/01/30 00:42:00 $
  *
