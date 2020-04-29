3c12a7931d509c5e3557c5ed44c9a32e9c917c7d
Andreas Gohr <andi@splitbrain.org>
2020-04-29 20:34:10 +0200

    fix: SF Bug 190: highlight_lines_extra eats end-of-line
    This applies the proposed fix from https://sourceforge.net/p/geshi/bugs/190/
    
    Because of the lack of comprehensive tests, it's unclear if this has any
    side effects, but it does fix the reported problem.
    
    A test case for this bug in particular has been added.

---
+++ tests/NewlineTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Ensures the number of output lines are the same as the input
+ */
+class NewlineTest extends TestCase
+{
+
+    /**
+     * No fancy settings
+     */
+    public function testDefault()
+    {
+        $input = <<< END
+void main () {
+    printf ("Hello World");
+    exit 0;
+}
+END;
+        $input_lines = count(explode("\n", $input));
+
+        $geshi = new GeSHi($input, 'c');
+        $geshi->highlight_lines_extra(2);
+        $output = $geshi->parse_code();
+        $output_lines = count(explode("\n", $output));
+
+        $this->assertEquals($input_lines, $output_lines, "number of line mismatch between input and output");
+    }
+
+    /**
+     * Highlighting a line
+     *
+     * checks for SF bug 190
+     *
+     * @link https://sourceforge.net/p/geshi/bugs/190/
+     */
+    public function testHighlight()
+    {
+        $input = <<< END
+void main () {
+    printf ("Hello World");
+    exit 0;
+}
+END;
+        $input_lines = count(explode("\n", $input));
+
+        $geshi = new GeSHi($input, 'c');
+        $geshi->highlight_lines_extra(2);
+        $output = $geshi->parse_code();
+        $output_lines = count(explode("\n", $output));
+
+        $this->assertEquals($input_lines, $output_lines, "number of line mismatch between input and output");
+    }
+}
