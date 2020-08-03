76e7d0b03d2eb99f8f06d5d9225d69d5826f4853
Chuck Nemeth <chuckn246@gmail.com>
2020-08-03 15:59:50 -0400

    Add pvesm

--- src/geshi/bash.php
+++ src/geshi/bash.php
@@ -323,6 +323,13 @@ $language_data = array (
             'pveam download', 'pveam available', 'pveam list', 'pveam remove',
             'pveam update', 'pveam help',
 
+            'pvesm scan cifs', 'pvesm scan glusterfs', 'pvesm scan iscsi',
+            'pvesm scan lvm', 'pvesm scan lvmthin', 'pvesm scan nfs', 'pvesm scan zfs',
+            'pvesm add', 'pvesm remove', 'pvesm set',
+            'pvesm alloc', 'pvesm free', 'pvesm list', 'pvesm status',
+            'pvesm apiinfo', 'pvesm export', 'pvesm extractconfig', 'pvesm import',
+            'pvesm path', 'pvesm help',
+
             'rbash', 'rcs', 'rcs2log', 'readlink', 'red', 'resizecons',
             'rev', 'rm', 'rmdir', 'rsh', 'run-parts',
 
